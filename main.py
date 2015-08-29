0edd298c2a6d47ad9f563a736834a06ad3683401
Ali Esmaili <ae.seraji@gmail.com>
2015-08-29 20:47:05 +0430

    Intro , Titr and Gallery added to GetData

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 import re
 import httplib
+import os
 
 robots_txt = """User-agent: *Disallow: /resources/
 		Disallow: /_resources/
@@ -12,6 +13,34 @@ robots_txt = """User-agent: *Disallow: /resources/
 
 def main():
 	""" the Program starts Here. . ."""
+	if False:
+		html = """
+<ul style="left: 0px; top: 0px;">
+																							
+												<li oncontextmenu="return false;" class="gallerypic group1 cboxElement" style="width: 256.667px; height: 170px;" href="/_uploads/Gallery/gallery_3491/20150117200018_7.jpg" title=""><img src="/_uploads/Gallery/gallery_3491/20150117200018_7.jpg" alt="دخمه سنگی کافر کلی" oncontextmenu="return false;"></li>
+																							
+												<li oncontextmenu="return false;" class="gallerypic group1 cboxElement" style="width: 256.667px; height: 170px;" href="/_uploads/Gallery/gallery_3491/20150117200018_6.jpg" title=""><img src="/_uploads/Gallery/gallery_3491/20150117200018_6.jpg" alt="دخمه سنگی کافر کلی" oncontextmenu="return false;"></li>
+																							
+												<li oncontextmenu="return false;" class="gallerypic group1 cboxElement" style="width: 256.667px; height: 170px;" href="/_uploads/Gallery/gallery_3491/20150117200018_5.jpg" title=""><img src="/_uploads/Gallery/gallery_3491/20150117200018_5.jpg" alt="دخمه سنگی کافر کلی" oncontextmenu="return false;"></li>
+																							
+												<li oncontextmenu="return false;" class="gallerypic group1 cboxElement" style="width: 256.667px; height: 170px;" href="/_uploads/Gallery/gallery_3491/20150117200019_4.jpg" title=""><img src="/_uploads/Gallery/gallery_3491/20150117200019_4.jpg" alt="دخمه سنگی کافر کلی" oncontextmenu="return false;"></li>
+																							
+												<li oncontextmenu="return false;" class="gallerypic group1 cboxElement" style="width: 256.667px; height: 170px;" href="/_uploads/Gallery/gallery_3491/20150117200019_3.jpg" title=""><img src="/_uploads/Gallery/gallery_3491/20150117200019_3.jpg" alt="دخمه سنگی کافر کلی" oncontextmenu="return false;"></li>
+																							
+												<li oncontextmenu="return false;" class="gallerypic group1 cboxElement" style="width: 256.667px; height: 170px;" href="/_uploads/Gallery/gallery_3491/20150117200019_2.jpg" title=""><img src="/_uploads/Gallery/gallery_3491/20150117200019_2.jpg" alt="دخمه سنگی کافر کلی" oncontextmenu="return false;"></li>
+																							
+												<li oncontextmenu="return false;" class="gallerypic group1 cboxElement" style="width: 256.667px; height: 170px;" href="/_uploads/Gallery/gallery_3491/20150117200019_1.jpg" title=""><img src="/_uploads/Gallery/gallery_3491/20150117200019_1.jpg" alt="دخمه سنگی کافر کلی" oncontextmenu="return false;"></li>
+																						</ul>
+		"""
+		html = whiteSpaceDel(html)
+		regImg = r'<li.*?class="gallerypic group1 cboxElement".*?>.*?<img.*?src="(.*?)".*?>.*?</li>'
+		c = 0
+		for match in re.finditer(regImg, html):
+			src = match.group(1)
+			log("[*]Downloading %s..." % src )
+		return 0;
+
+
 	#xmlData = request("http://www.bezanimbiroon.ir", "DataBase")
 	log("[*] Starting....")
 
@@ -121,6 +150,64 @@ def getData(address, name , distance = 0):
 	html = response.read()
 	html = whiteSpaceDel(html)
 
+
+	# Titr regular expression:
+	regTitr = r'<div class="locIntroTextTitr">(.*?)</div>'
+	match = re.search(regTitr, html)
+	Title = match.group(1)
+	if match:
+		ret += "\n<title>%s</title>\n" % Title
+	else:
+		log("[!]: Can not find Title...." , distance)
+
+
+	# Intro regular expressoin:
+	regIntro = r'<div class="locIntroTextBody">.*?<p>(.*?)</p>.*?</div>'
+	match = re.search(regIntro, html)
+	if match:
+		ret += "\n<Intro>\n%s\n</Intro>\n" % match.group(1)
+	else:
+		log("[!]: Can not find Intro text...", distance)
+
+	p = file("h.html","wb")
+	p.write(html)
+	p.close()
+
+	# Gallery regular expression:
+	regImg = r'<li.*?class="gallerypic group1".*?>.*?<img.*?src="(.*?)".*?>.*?</li>'
+
+	# setting Folder Name:
+	folderName = Title;
+	if folderName == "":
+		folderName = name
+	if folderName =="":
+		folderName = "others"
+
+	# make sure exists?
+	if not os.path.exists(folderName):
+		os.makedirs(folderName)
+
+	# ... and Download imgs:
+	for match in re.finditer(regImg, html):
+		src = match.group(1)
+		log("[*]Downloading %s..." % getRelativeAddress(src) , distance)
+		imgDwConn = httplib.HTTPConnection("www.bezanimbiroon.ir")
+		imgDwConn.request("GET",getRelativeAddress(src))	
+		res = imgDwConn.getresponse()
+		if res.status != 200:
+			log("[!] Cant Download image file" , distance)
+			continue
+		fileName = re.match(r'(?:.*)/(.*?)$' , src).group(1)
+		imgFile = res.read()
+		img = file("%s/%s" % (folderName, fileName) , "wb")
+		img.write(imgFile)
+		img.close()
+		ret += "\n<img>%s/%s</img>\n" % (folderName, fileName)
+		log("[+]Downloaded Complite: %s" % src , distance)
+
+
+
+
 	# Properties regular expression:
 	reg11 = r'<div id=[\'\"]Ltab1[\'\"]>.*<div.*>(.*)</div>.*<div class="OtherProperties">(.*)</div>.*</div>'
 	reg12 = r'<div class="OtherPropertiesRow.">(.*?)<img .*?>.*?</div>\s?<div class="OtherPropertiesRowContent">(.*?)</div>'
@@ -141,6 +228,8 @@ def getData(address, name , distance = 0):
 
 	for match in re.finditer(reg12, prop):
 		ret += "\n<%s>\n%s\n</%s>\n" % (match.group(1).strip(), match.group(2).strip(), match.group(1).strip())
+
+
 
 	# Feature regular expression:
 	reg21 = r''
